ff00aae787f063341bbc5e41042796a864dd0252
Vitor Ferreira Iassia <iassiavitor@gmail.com>
2021-10-08 12:48:16 -0300

    eslint + hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [resret] = useState(false);
+  const handleReverseClick = (event) => {
+    event.preventDefault();
+    setReverse(!reverse);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={reverse ? 'reverse' : 'App-logo'} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
+          onClick={handleReverseClick}
           target="_blank"
           rel="noopener noreferrer"
         >
